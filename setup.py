f35cc091eb82e3f0c4110fd8cd45aaec2fd8560f
Tom Milligan <tom@tommilligan.net>
2017-03-14 21:57:00 +0000

    Added coveralls requirement for reporting

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ setup(
             'sphinxcontrib-httpdomain ~= 1.5.0',
             'codeclimate-test-reporter ~= 0.2.1',
             'cov-core ~= 1.15.0',
-            'nose2 ~= 0.6.5'
+            'nose2 ~= 0.6.5',
+            'coveralls ~= 1.1''
         ]
     },
     entry_points={
